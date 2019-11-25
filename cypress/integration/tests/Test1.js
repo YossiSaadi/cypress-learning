ca2eeb5af695a4797b334d9f91a9914ce6c0ca04
YossiSaadi <yossi.saadi@duda.co>
2019-11-25 13:08:17 +0200

    minor

--- cypress/integration/tests/Test1.js
+++ cypress/integration/tests/Test1.js
@@ -1,16 +1,68 @@
 describe('My First Test', () => {
-  it('My First testcase - should do nothing special to do nothing', () => {
+  it('My First testcase - should do nothing special to do nothing', async () => {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise');
 
-    const searchBox = cy.get(`input[type='search']`);
-    searchBox.click();
-    searchBox.type("ca");
+    // cy.get(`input[type='search']`).then(($input) => {
+    //   const input = cy.wrap($input);
+    //   input.click().type('ca');
+    //
+    //   cy.get('.products').then(($products) => {
+    //     debugger;
+    //
+    //     $products.find('.product').each(($el, index, $list) => {
+    //       debugger;
+    //       const elementText = cy.wrap($el).find('.product-name').text();
+    //       if (elementText.includes('Carrot')) {
+    //         cy.wrap($el).click();
+    //       }
+    //     });
+    //   })
+    // });
 
-    const products = cy.get(`.product`).should('have.length', 4);
-    console.log(products);
-
-  });
-  it('My Second testcase - should do nothing special to do nothing', () => {
-      
+    cy.get(`input[type='search']`).type('ca');
+    cy.wait(2000);
+    cy.get('.products')
+      .find('.product')
+      .each(($product, index, $list) => {
+        const name = $product.find('.product-name').text();
+        if (name.includes('Capsi')) {
+          cy.wrap($product).find('button').click().should('be.eq', 'ADDED');
+          cy.get('').should('')
+        }
+      });
   });
 });
+
+// const iterateByName = (list, name) => {
+//   list.each(($element, index, $list) => {
+//     const name = $element.text();
+//     if ($element.includes(name)) {
+//       return cy.wrap($element);
+//     }
+//   });
+//   return null;
+// };
+
+
+// const products = cy.get(`.product`).should('have.length', 4);
+
+// const products = cy.get(`.product`);
+// const visibleProducts = products.get(':visible');
+// visibleProducts.should('have.length', 4);
+
+// const productsParent = cy.get('.products');
+// const productsFromProductsParent = productsParent.find('.product');
+// productsFromProductsParent.should('have.length', 4);
+// productsFromProductsParent.contains("Carrot").click();
+// productsFromProductsParent.eq(2).contains("ADD TO CART").click().should('be.eq', 'Added');
+
+// const productsParent = await cy.get('.products');
+// const productsFromProductsParent = await productsParent.find('.product');
+
+// productsFromProductsParent.each(($el, index, $list) => {
+//   if ($el.find('product-name').text().includes('Carrot')) {
+//     cy.wrap($el).click();
+//   } else {
+//     throw new Error("Not Found");
+// }
+// });
